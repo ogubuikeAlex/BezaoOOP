da0c2e3ef8ba3bc5f0e24c2faafc8102d170f283
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-11-18 19:17:24 +0100

    Added Code for final OOP class

---
+++ LearningPolymorphism.cs
@@ -0,0 +1,33 @@
+﻿namespace BezaoOOP
+{
+    internal class LearningPolymorphism
+    {
+        public int Add()
+        {
+            return 5 + 6;
+        }
+      
+        public int Add( string number, int num)
+        {
+            return num + 6;
+        }
+
+        public int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public virtual int Subtract (int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+    }
+
+    class Mathematics : LearningPolymorphism
+    {
+        public override int  Subtract(int numberOne, int numberTwo)
+        {
+            return numberTwo - numberOne;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,10 +8,35 @@ public class Program
         storeItem.DisplayStoreItem();*/
 
         //Application.Run();
-        PlayGround.Run();
+        //PlayGround.Run();
+
+        //PlayGroundThree.Run();
 
       /*  StoreItem storeItemTwo = new StoreItem(5000, "Laptop", "WriteAnything");
         System.Console.WriteLine("\nParameterized Sample:");
         storeItemTwo.DisplayStoreItem();*/
+
+
+
+
+        LearningInterfaces learningInterfaces = new LearningInterfaces();
+
+        IPerson student = new Student();
+        IPerson teacher = new Teacher();
+        IPerson admin = new Admin();
+
+        learningInterfaces.OnStudy(student, "I dont like to read!!!!");
+        learningInterfaces.OnBreak(student, "I love to play");
+        learningInterfaces.OnWrite(student, "Na God go understand this handwriting");
+
+        learningInterfaces.OnStudy(teacher, "Hmmmmmmmm!!!!");
+        learningInterfaces.OnBreak(teacher, "I need to study more");
+        learningInterfaces.OnWrite(teacher, "What a handwriting🙂");
+
+        learningInterfaces.OnStudy(admin, "I don pass this level");
+        learningInterfaces.OnBreak(admin, "Jaiye jaiye");
+        learningInterfaces.OnWrite(admin, "Query to Nneka!!");
+
+
     }
 }
